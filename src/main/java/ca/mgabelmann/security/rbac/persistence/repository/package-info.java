048ee37f0f68239f615373143dfa699abc75ac0f
mgabelmann <mgabelmann@gmail.com>
2024-07-12 14:55:40 -0700

    added package-info.java added github build files

---
+++ src/main/java/ca/mgabelmann/security/rbac/persistence/repository/package-info.java
@@ -0,0 +1,1 @@
+package ca.mgabelmann.security.rbac.persistence.repository;
--- src/test/java/ca/mgabelmann/security/rbac/persistence/repository/OrganizationRepositoryTest.java
+++ src/test/java/ca/mgabelmann/security/rbac/persistence/repository/OrganizationRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
